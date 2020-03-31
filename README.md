c77a0b413fc67501559c3bce3dc636bcbf7cf9e0
Madara <guesswecan@163.com>
2020-03-31 15:27:09 +0800

    07 move
    07 move

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Game Development in Rust
  
-windows 64位 环境：
+**windows 64位 环境：**
 
 1. https://www.libsdl.org/download-2.0.php 下载 SDL2-devel-2.0.12-VC.zip
 
@@ -9,13 +9,13 @@ windows 64位 环境：
 
    SDL2.dll，放入Cargo.toml同级目录
 
-2. https://www.libsdl.org/projects/SDL_image/ 下载 SDL2_image-devel-2.0.5-VC.zip
+2. https://www.libsdl.org/projects/SDL_image 下载 SDL2_image-devel-2.0.5-VC.zip
 
    下载完解压，SDL2_image.lib 放入，
    .rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\x86_64-pc-windows-msvc\lib
 
    文件夹内dll，放入Cargo.toml同级目录
 
-运行：
+**运行：**
 
 cargo run --example 01_window
---
+++ examples/06_move1.rs
@@ -0,0 +1,100 @@
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::render::{WindowCanvas, Texture};
+use sdl2::rect::{Point, Rect};
+// "self" imports the "image" module itself as well as everything else we listed
+use sdl2::image::{self, LoadTexture, InitFlag};
+use std::time::Duration;
+
+#[derive(Debug)]
+struct Player {
+    position: Point,
+    sprite: Rect,
+    speed: i32,
+}
+
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    player: &Player,
+) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    let (width, height) = canvas.output_size()?;
+
+    // Treat the center of the screen as the (0, 0) coordinate
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
+    canvas.copy(texture, player.sprite, screen_rect)?;
+
+    canvas.present();
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
+    // stay unused because if we don't have any variable at all then Rust will treat it as a
+    // temporary value and drop it right away!
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem.window("game tutorial", 800, 600)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let mut canvas = window.into_canvas().build()
+        .expect("could not make a canvas");
+
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let mut player = Player {
+        position: Point::new(0, 0),
+        sprite: Rect::new(0, 0, 26, 36),
+        speed: 5,
+    };
+
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut i = 0;
+    'running: loop {
+        // Handle events
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
+                    player.position = player.position.offset(-player.speed, 0);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
+                    player.position = player.position.offset(player.speed, 0);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
+                    player.position = player.position.offset(0, -player.speed);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
+                    player.position = player.position.offset(0, player.speed);
+                },
+                _ => {}
+            }
+        }
+
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
+
+        // Time management!
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    Ok(())
+}
